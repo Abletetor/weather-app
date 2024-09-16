07bdcc1463e70587032415ede98f64c15c447153
Abletetor <abletetor@gmail.com>
2024-09-16 14:21:59 +0000

    Full Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Weather from "./Component/Weather/Weather";
+
+const App = () => {
+   return (
+      <div className='app'>
+         <Weather />
+      </div>
+   );
+};
+
+export default App;
